722b71999e819634201a061e178830780e9f78df
Gladson Ramos <gladson.ramos@hotmail.com>
2023-01-07 11:58:20 -0400

    bugfix

--- samba.sh
+++ samba.sh
@@ -25,14 +25,14 @@ install(){
     sudo apt-get install acl attr autoconf bind9utils bison build-essential \
     debhelper dnsutils docbook-xml docbook-xsl flex gdb libjansson-dev krb5-user \
     libacl1-dev libaio-dev libarchive-dev libattr1-dev libblkid-dev libbsd-dev \
-    libcap-dev libcups2-dev libgnutls28-dev libgpgme-dev libjson-perl libldap2-dev \
-    libncurses5-dev libpam0g-dev libparse-yapp-perl libpopt-dev libreadline-dev \
-    nettle-dev perl perl-modules-5.30 pkg-config  python-all-dev python-crypto python2-dbg \
-    python-dev-is-python2 python-dnspython python3-dnspython python2-gpg python3-gpg \
-    python-markdown python3-markdown python3-dev xsltproc zlib1g-dev liblmdb-dev \
-    lmdb-utils acl attr samba samba-dsdb-modules samba-vfs-modules winbind krb5-config \
-    krb5-user dnsutils
-    
+    libcap-dev libcups2-dev libgnutls28-dev libgpgme-dev libjson-perl \
+    libldap2-dev libncurses5-dev libpam0g-dev libparse-yapp-perl \
+    libpopt-dev libreadline-dev nettle-dev perl perl-modules-5.30 pkg-config \
+    python-all-dev python-crypto python2-dbg python-dev-is-python2 python-dnspython \
+    python3-dnspython python3-gpg python-markdown python3-markdown \
+    python3-dev xsltproc zlib1g-dev liblmdb-dev lmdb-utils
+
+
     line
     echo "Preparing the installation"
     sudo mv /etc/samba/smb.conf /etc/samba/smb.conf.bkp
